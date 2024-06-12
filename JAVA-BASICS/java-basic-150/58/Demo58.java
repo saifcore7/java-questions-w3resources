1e58c83ce03ddbfc6127bb972072c016d0f171bb
saifcore7 <saif7862254j@gmail.com>
2024-06-12 23:33:32 +0530

    commit

--- JAVA-BASICS/java-basic-150/58/Demo58.java
+++ JAVA-BASICS/java-basic-150/58/Demo58.java
@@ -1,5 +1,19 @@
+/*58. Write a Java program to capitalize the first letter of each word in a sentence.
+Sample Output:
+
+Input a Sentence: the quick brown fox jumps over the lazy dog.         
+The Quick Brown Fox Jumps Over The Lazy Dog. */
+
 public class Demo58{
     public static void main(String[] args) {
-        System.out.println("hello");
+        String str = "the quick brown fox jumps over the lazy dog";
+        char[] ch = str.toCharArray();
+        ch[0] = Character.toUpperCase(ch[0]);
+        for(int i = 1; i < ch.length; i++) {
+            if(ch[i] == ' ') {
+                ch[i+1] = Character.toUpperCase(ch[i+1]);
+            }
+        }
+        System.out.println(ch);
     }
 }
