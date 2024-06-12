2270230be7e5c810073cdd80404ea3a8ec4e3ebb
saifcore7 <saif7862254j@gmail.com>
2024-06-12 23:34:46 +0530

    commit

--- JAVA-BASICS/java-basic-150/57/Demo57.java
+++ JAVA-BASICS/java-basic-150/57/Demo57.java
@@ -1,5 +1,20 @@
-public class Demo57{
+/*
+ 57. Write a Java program to accept an integer and count the factors of the number.
+Sample Output:
+
+Input an integer: 25                                                   
+3
+
+ */
+
+public class Demo57 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        int n = 30, factors = 0;
+        for (int i = 1; i <= n; i++) {
+            if(n % i == 0) {
+                factors++;
+            }
+        }
+        System.out.println(factors);
     }
 }
