ea7c26b44df6a43defe3ce15d627877e021de1ec
saifcore7 <saif7862254j@gmail.com>
2024-06-15 20:21:25 +0530

    commit

---
+++ automation/push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# path to the local repo
+cd /home/saifcore7/Desktop/java-questions-w3resources
+
+# stage all changes 
+git add .
+
+#commit the changes
+git commit -m "commit"
+
+#push to the remote repo
+git push -u origin main
