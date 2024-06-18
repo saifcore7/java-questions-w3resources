51df35e86631feae38f4894893ec4cf7ae610fbe
saifcore7 <saif7862254j@gmail.com>
2024-06-19 00:43:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/71/Demo71.java
+++ JAVA-BASICS/java-basic-150/71/Demo71.java
@@ -7,6 +7,6 @@ public class Demo71{
     public static void main(String[] args) {
         String str1 = "Python";
         String str2 = "Tutorial";
-        System.out.println(str1.substring(1,str1.length()));
+        System.out.println(str1.substring(1,str1.length()) + str2.substring(1,str2.length()));
     }
 }
