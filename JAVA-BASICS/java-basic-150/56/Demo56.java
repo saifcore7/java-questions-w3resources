98f4d92a85d1c71b7dcdd276d26749997e76a2ba
saifcore7 <saif7862254j@gmail.com>
2024-06-18 23:17:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/56/Demo56.java
+++ JAVA-BASICS/java-basic-150/56/Demo56.java
@@ -1,3 +1,10 @@
+/*56. Write a Java program to find the number of values in a given range divisible by a given value.
+For example x = 5, y=20 and p =3, find the number of integers within the range x..y and that are divisible by p i.e. { i :x ≤ i ≤ y, i mod p = 0 }
+Sample Output:
+
+5
+ */
+
 public class Demo56{
     public static void main(String[] args) {
         System.out.println("hello");
