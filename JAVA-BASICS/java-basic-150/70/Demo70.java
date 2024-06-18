a2856f0778e9eda1ab102171301874e500c755c8
saifcore7 <saif7862254j@gmail.com>
2024-06-19 00:38:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/70/Demo70.java
+++ JAVA-BASICS/java-basic-150/70/Demo70.java
@@ -1,3 +1,10 @@
+/*70. Write a Java program to create a string in the form of short_string + long_string + short_string from two strings. The strings must not have the same length.
+Test Data: Str1 = Python
+Str2 = Tutorial
+Sample Output:
+
+PythonTutorialPython */
+
 public class Demo70{
     public static void main(String[] args) {
         System.out.println("hello");
