1faee1dfe366abe053a164c7d22ebc23b6acb24d
saifcore7 <saif7862254j@gmail.com>
2024-06-19 00:37:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/69/Demo69.java
+++ JAVA-BASICS/java-basic-150/69/Demo69.java
@@ -6,6 +6,6 @@ Pyt */
 public class Demo69{
     public static void main(String[] args) {
         String str = "Python";
-        System.out.println((str.length() % 2 == 0)?"First half of the string is : " + str.substring(0, str.length() / 2):"String is not even");
+        System.out.println((str.length() % 2 == 0)?"First half of the " + str + " string is : " + str.substring(0, str.length() / 2):"String is not even");
     }
 }
