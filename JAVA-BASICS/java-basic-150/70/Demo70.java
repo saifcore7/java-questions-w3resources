a30dc3f573c2b576d8192adad86770ef5de4cbc5
saifcore7 <saif7862254j@gmail.com>
2024-06-19 00:40:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/70/Demo70.java
+++ JAVA-BASICS/java-basic-150/70/Demo70.java
@@ -9,6 +9,6 @@ public class Demo70{
     public static void main(String[] args) {
         String str1 = "python";
         String str2 = "tutorial";
-        System.out.println((str1.length() > str2.length()));
+        System.out.println((str1.length() > str2.length())?str2 + str1 + str2:"test");
     }
 }
