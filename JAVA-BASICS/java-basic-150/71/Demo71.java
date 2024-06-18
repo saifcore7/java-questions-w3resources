f23298a6a52f4a25936613649fab259ba59cc5a9
saifcore7 <saif7862254j@gmail.com>
2024-06-19 00:42:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/71/Demo71.java
+++ JAVA-BASICS/java-basic-150/71/Demo71.java
@@ -1,5 +1,12 @@
+/*71. Write a Java program to create the concatenation of the two strings except removing the first character of each string. The length of the strings must be 1 and above.
+Test Data: Str1 =  Python
+Str2 = Tutorial
+Sample Output: */
+
 public class Demo71{
     public static void main(String[] args) {
-        System.out.println("hello");
+        String str1 = "Python";
+        String str2 = "Tutorial";
+        System.out.println(str1.substring(1,str1.length()));
     }
 }
