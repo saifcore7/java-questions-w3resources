05be45f2d056f65a5dcfb456cc7d93e0c1b3dc58
saifcore7 <saif7862254j@gmail.com>
2024-06-18 23:10:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/55/Demo55.java
+++ JAVA-BASICS/java-basic-150/55/Demo55.java
@@ -1,5 +1,13 @@
+/*55. Write a Java program to convert seconds to hours, minutes and seconds.
+Sample Output:
+
+Input seconds: 86399                                                   
+23:59:59
+ */
+
 public class Demo55{
     public static void main(String[] args) {
-        System.out.println("hello");
+        int s = 86399;
+        int m = 86399 / 60;
     }
 }
