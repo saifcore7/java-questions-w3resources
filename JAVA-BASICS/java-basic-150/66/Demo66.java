a263d8a71e23424dd6a75aad07c9792cc84e5665
saifcore7 <saif7862254j@gmail.com>
2024-06-19 00:29:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/66/Demo66.java
+++ JAVA-BASICS/java-basic-150/66/Demo66.java
@@ -5,10 +5,11 @@ Sum of the first 100 prime numbers: 24133  */
 
 public class Demo66{
     public static void main(String[] args) {
-        int count = 0;
+        int count;
         int s = 0;
         for(int i = 2; i < 10; i++) {
             for(int j = 1; j < i; j++) {
+                count = 0;
                 if (i%j==0) {
                     count++;
                 }
