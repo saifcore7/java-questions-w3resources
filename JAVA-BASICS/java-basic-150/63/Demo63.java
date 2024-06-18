1c18badb589c58d56110025088b10aa18e3448f7
saifcore7 <saif7862254j@gmail.com>
2024-06-18 23:45:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/63/Demo63.java
+++ JAVA-BASICS/java-basic-150/63/Demo63.java
@@ -9,9 +9,13 @@ Result: 13
 public class Demo63{
 
     public boolean largest(int a, int b) {
-        
+        if(a==b) {
+            return 0;
+        }
     }
     public static void main(String[] args) {
-        
+        Demo63 obj = new Demo63();
+        boolean result = obj.largest(12,13);
+        System.out.println(result);
     }
 }
