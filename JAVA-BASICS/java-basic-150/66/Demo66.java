344234573dc111ed8754ef204db8fdef9865e225
saifcore7 <saif7862254j@gmail.com>
2024-06-18 23:57:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/66/Demo66.java
+++ JAVA-BASICS/java-basic-150/66/Demo66.java
@@ -1,5 +1,10 @@
+/*66. Write a Java program to compute the sum of the first 100 prime numbers.
+Sample Output:
+
+Sum of the first 100 prime numbers: 24133  */
+
 public class Demo66{
     public static void main(String[] args) {
-        System.out.println("hello");
+        for(int i = 2;)
     }
 }
