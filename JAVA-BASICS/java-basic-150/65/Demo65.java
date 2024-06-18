540b998255c8573bbdd0e97681cfea7c446f6d48
saifcore7 <saif7862254j@gmail.com>
2024-06-18 23:56:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/65/Demo65.java
+++ JAVA-BASICS/java-basic-150/65/Demo65.java
@@ -1,5 +1,15 @@
+/*65. Write a Java program to calculate the modules of two numbers without using any inbuilt modulus operator.
+Sample Output:
+
+Input the first number : 19                                            
+Input the second number: 7                                             
+5   
+ */
+
 public class Demo65{
     public static void main(String[] args) {
-        System.out.println("hello");
+        int a = 19;
+        int b = 7;
+        System.out.println(a - (b * (19/7)));
     }
 }
