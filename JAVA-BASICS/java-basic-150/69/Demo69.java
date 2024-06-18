6a9b694bb75e59a9f23c08e4bff0bf7629c6a7e8
saifcore7 <saif7862254j@gmail.com>
2024-06-19 00:33:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/69/Demo69.java
+++ JAVA-BASICS/java-basic-150/69/Demo69.java
@@ -1,3 +1,8 @@
+/*69. Write a Java program to extract the first half of a even string.
+Test Data: Python
+Sample Output:
+Pyt */
+
 public class Demo69{
     public static void main(String[] args) {
         System.out.println("hello");
