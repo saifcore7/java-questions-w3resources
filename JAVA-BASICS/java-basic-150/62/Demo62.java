d743d10e025117add702ff63fb546f3cebc5451e
saifcore7 <saif7862254j@gmail.com>
2024-06-18 23:23:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/62/Demo62.java
+++ JAVA-BASICS/java-basic-150/62/Demo62.java
@@ -1,5 +1,13 @@
+/*62. Write a Java program that accepts three integer values and returns true if one is 20 or more less than the others' subtractions.
+Sample Output:
+
+Input the first number : 15                                            
+Input the second number: 20                                            
+Input the third number : 25                                            
+false */
+
 public class Demo62{
     public static void main(String[] args) {
-        System.out.println("hello");
+        int a = 15, b = 20, c= 25;
     }
 }
