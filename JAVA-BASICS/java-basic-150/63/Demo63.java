2f2f369fb72d3836945ef3362756c26e0e0e6257
saifcore7 <saif7862254j@gmail.com>
2024-06-18 23:42:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/63/Demo63.java
+++ JAVA-BASICS/java-basic-150/63/Demo63.java
@@ -1,5 +1,13 @@
+/*63. Write a Java program that accepts two integer values from the user and returns the largest value. However if the two values are the same, return 0 and find the smallest value if the two values have the same remainder when divided by 6.
+Sample Output:
+
+Input the first number : 12                                            
+Input the second number: 13                                            
+Result: 13
+ */
+
 public class Demo63{
     public static void main(String[] args) {
-        System.out.println("hello");
+        
     }
 }
