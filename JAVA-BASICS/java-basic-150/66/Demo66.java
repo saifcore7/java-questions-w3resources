25e25b86f714e3c29e68167dd35fe1dcf24c839e
saifcore7 <saif7862254j@gmail.com>
2024-06-19 00:05:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/66/Demo66.java
+++ JAVA-BASICS/java-basic-150/66/Demo66.java
@@ -7,7 +7,7 @@ public class Demo66{
     public static void main(String[] args) {
         int s = 0;
         for(int i = 2; i <=100; i++) {
-            for(int j = 1; j<=i; j++) {
+            for(int j = 2; j<=i; j++) {
 
             }
         }
