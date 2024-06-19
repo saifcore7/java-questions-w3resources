838bddbce85ad8f016f3e3667efdbd6090d598c4
saifcore7 <saif7862254j@gmail.com>
2024-06-19 13:06:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/74/Demo74.java
+++ JAVA-BASICS/java-basic-150/74/Demo74.java
@@ -1,5 +1,9 @@
+/*74. Write a Java program to test if 10 appears as the first or last element of an array of integers. The array length must be broader than or equal to 2.
+Sample Output:
+Test Data: array = 10, -20, 0, 30, 40, 60, 10 */
+
 public class Demo74{
     public static void main(String[] args) {
-        System.out.println("hello");
+       
     }
 }
