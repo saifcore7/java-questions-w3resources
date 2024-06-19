a800e7aa25f2082ba81f24faf147491b2163ffc5
saifcore7 <saif7862254j@gmail.com>
2024-06-19 16:04:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/77/Demo77.java
+++ JAVA-BASICS/java-basic-150/77/Demo77.java
@@ -11,12 +11,12 @@ public class Demo77 {
     public static void main(String[] args) {
         int[] a1 = { 50, -20, 0 };
         int[] a2 = { 5, -50, 10 };
-        int[] a3 = {};
+        int[] a3 = new int[2];
         for (int i = 0; i < 2; i++) {
             if (i == 0)
                 a3[0] = a1[0];
             if (i == 1)
-                a3[1] = a2[a2.length - 1];
+                a3[0] = a2[a2.length - 1];
         }
         for (int i = 0; i < 2; i++) {
             System.out.println(a3[i] + " ");
