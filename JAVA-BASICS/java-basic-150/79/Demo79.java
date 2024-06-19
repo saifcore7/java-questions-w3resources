92516649b75997a1505924bd376fcdfb8f475100
saifcore7 <saif7862254j@gmail.com>
2024-06-19 16:10:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/79/Demo79.java
+++ JAVA-BASICS/java-basic-150/79/Demo79.java
@@ -1,3 +1,9 @@
+/*79. Write a Java program to rotate an array (length 3) of integers in the left direction.
+Sample Output:
+
+Original Array: [20, 30, 40]                                           
+Rotated Array: [30, 40, 20] */
+
 public class Demo79{
     public static void main(String[] args) {
         System.out.println("hello");
