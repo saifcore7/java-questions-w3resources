7677ecb7850e529c28fe084e837cb943b83b1c0c
saifcore7 <saif7862254j@gmail.com>
2024-06-19 13:26:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/75/Demo75.java
+++ JAVA-BASICS/java-basic-150/75/Demo75.java
@@ -1,5 +1,11 @@
+/*75. Write a Java program to test if the first and last elements of an array of integers are the same. The array length must be broader than or equal to 2.
+Test Data: array = 50, -20, 0, 30, 40, 60, 10
+Sample Output:
+
+false */
+
 public class Demo75{
     public static void main(String[] args) {
-        System.out.println("hello");
+        
     }
 }
