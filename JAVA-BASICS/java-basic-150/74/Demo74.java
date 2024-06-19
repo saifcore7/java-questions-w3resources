020071e48ea2ed54cc44bdec3d6036afcaba5de7
saifcore7 <saif7862254j@gmail.com>
2024-06-19 13:15:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/74/Demo74.java
+++ JAVA-BASICS/java-basic-150/74/Demo74.java
@@ -9,14 +9,17 @@ public class Demo74{
 
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
-       System.out.println("Enter the size of array : ");
+       System.out.print("Enter the size of array : ");
        int SIZE = sc.nextInt();
        int a[] = new int[SIZE];
        if(SIZE >= 2) {
         for(int i = 0; i < SIZE; i++) {
             if(a[0] == 10 && a[SIZE - 1] == 10) System.out.println("true");
+            else System.out.println("false");
 
         }
        }
+       else System.out.println("Size of array is not greater than or equal to 2");
+       sc.close();
     }
 }
