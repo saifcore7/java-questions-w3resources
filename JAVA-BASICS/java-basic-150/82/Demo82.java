4ca667b05320af229e7cf6017b23f2a498ea88fb
saifcore7 <saif7862254j@gmail.com>
2024-06-26 23:16:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/82/Demo82.java
+++ JAVA-BASICS/java-basic-150/82/Demo82.java
@@ -1,3 +1,9 @@
+/*81. Write a Java program to swap the first and last elements of an array (length must be at least 1) and create another array.
+Sample Output:
+
+Original Array: [20, 30, 40]                                           
+New array after swaping the first and last elements: [40, 30, 20] */
+
 public class Demo82{
     public static void main(String[] args) {
         System.out.println("hello");
