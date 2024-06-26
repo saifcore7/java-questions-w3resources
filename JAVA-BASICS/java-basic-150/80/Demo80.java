c0c3c5e2d3bcef3d13ebb9bef697dfb2a9bf431d
saifcore7 <saif7862254j@gmail.com>
2024-06-26 23:11:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/80/Demo80.java
+++ JAVA-BASICS/java-basic-150/80/Demo80.java
@@ -1,5 +1,12 @@
+/*80. Write a Java program to get the largest value between the first and last elements of an array (length 3) of integers.
+Sample Output:
+
+Original Array: [20, 30, 40]                                           
+Larger value between first and last element: 40 */
+
 public class Demo80{
     public static void main(String[] args) {
-        System.out.println("hello");
+        int a[] = {20,30,40};
+
     }
 }
