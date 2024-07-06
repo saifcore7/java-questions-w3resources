256425a67ee3e79570b378841ecfed48c2cd3fdf
saifcore7 <saif7862254j@gmail.com>
2024-07-07 00:24:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/86/Demo86.java
+++ JAVA-BASICS/java-basic-150/86/Demo86.java
@@ -1,5 +1,9 @@
+/*86. Write a Java program starting with an integer n, 
+divide it by 2 if it is even, or multiply it by 3 and add 
+1 if it is odd. Repeat the process until n = 1.*/
+
 public class Demo86{
     public static void main(String[] args) {
-        System.out.println("hello");
+        
     }
 }
