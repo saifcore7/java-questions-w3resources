8f7e89349af96d2c6da102b64021381b4805c449
saifcore7 <saif7862254j@gmail.com>
2024-07-07 00:06:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/83/Demo83.java
+++ JAVA-BASICS/java-basic-150/83/Demo83.java
@@ -1,3 +1,12 @@
+/*83. Write a Java program to multiply the corresponding elements of two integer arrays.
+Sample Output:
+
+Array1: [1, 3, -5, 4]                                                  
+                                                                       
+Array2: [1, 4, -5, -2]                                                 
+                                                                       
+Result: 1 12 25 -8*/
+
 public class Demo83{
     public static void main(String[] args) {
         System.out.println("hello");
