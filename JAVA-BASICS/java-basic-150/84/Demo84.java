5a74ced012c5d30cf41d2d73867bd3fc30978b18
saifcore7 <saif7862254j@gmail.com>
2024-07-07 00:12:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/84/Demo84.java
+++ JAVA-BASICS/java-basic-150/84/Demo84.java
@@ -1,5 +1,11 @@
+/*84. Write a Java program to take the last three characters from a given string. It will add the three characters at both the front and back of the string. String length must be greater than three and more.
+Test data: "Python" will be "honPythonhon"
+Sample Output:
+
+honPythonhon*/
+
 public class Demo84{
     public static void main(String[] args) {
-        System.out.println("hello");
+        String str = "Python";
     }
 }
