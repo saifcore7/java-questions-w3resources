e49def8d3a66741a3b457c0c50df5ff41b08c44d
saifcore7 <saif7862254j@gmail.com>
2024-07-07 21:02:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/92/Demo92.java
+++ JAVA-BASICS/java-basic-150/92/Demo92.java
@@ -1,5 +1,9 @@
+/*92. Write a Java program to count the number 
+of even and odd elements in a given array of integers.
+Click me to see the solution*/
+
 public class Demo92{
     public static void main(String[] args) {
-        System.out.println("hello");
+        int[] a = {1,2,3,4,5,6,7,8,9,10};
     }
 }
