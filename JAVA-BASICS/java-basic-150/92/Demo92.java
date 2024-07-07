28d6fedfa3d3de3c04b7ebbb7f858a0b2a46593f
saifcore7 <saif7862254j@gmail.com>
2024-07-07 21:05:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/92/Demo92.java
+++ JAVA-BASICS/java-basic-150/92/Demo92.java
@@ -4,11 +4,12 @@ Click me to see the solution*/
 
 public class Demo92{
     public static void main(String[] args) {
-        int[] a = {1,2,3,4,5,6,7,8,9,10};
+        int[] a = {1,2,3,4,5,6,10};
         int even = 0,odd = 0;
         for(int i = 0; i < a.length; i++) {
             if(a[i] % 2 == 0) even++;
             else odd++;
         }
+        System.out.println(even + " " + odd);
     }
 }
