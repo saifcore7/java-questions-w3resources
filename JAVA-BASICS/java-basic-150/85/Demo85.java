817e2d8ab51d5bb11573e24bd3bf2fc5c5923619
saifcore7 <saif7862254j@gmail.com>
2024-07-07 20:29:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/85/Demo85.java
+++ JAVA-BASICS/java-basic-150/85/Demo85.java
@@ -1,5 +1,9 @@
+/*85. Write a Java program to check if a string starts with a specified word.
+Sample Data: string1 = "Hello how are you?"
+Sample Output:*/
+
 public class Demo85{
     public static void main(String[] args) {
-        System.out.println("hello");
+        
     }
 }
