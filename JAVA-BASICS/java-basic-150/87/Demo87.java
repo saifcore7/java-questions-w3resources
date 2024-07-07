7c0f163e8b70443bf591c457152882845a2939bd
saifcore7 <saif7862254j@gmail.com>
2024-07-07 20:34:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/87/Demo87.java
+++ JAVA-BASICS/java-basic-150/87/Demo87.java
@@ -1,5 +1,11 @@
+/*87. Write a Java program that then reads an integer and 
+calculates the sum of its digits and writes the number of 
+each digit of the sum in English.
+*/
+import java.util.*;
 public class Demo87{
     public static void main(String[] args) {
-        System.out.println("hello");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
     }
 }
