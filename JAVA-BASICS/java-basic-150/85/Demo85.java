4544e7c0f5c7b4a5c9772fe93f33c50450a66bab
saifcore7 <saif7862254j@gmail.com>
2024-07-07 20:31:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/85/Demo85.java
+++ JAVA-BASICS/java-basic-150/85/Demo85.java
@@ -4,7 +4,7 @@ Sample Output:*/
 
 public class Demo85{
     public static void main(String[] args) {
-        Strig str = "Hello how are you ?";
+        String str = "Hell how are you ?";
         System.out.println(str.startsWith("Hello"));
     }
 }
