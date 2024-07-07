1f310f9ac3edea8e1a0aebc97f76622e3dca1d91
saifcore7 <saif7862254j@gmail.com>
2024-07-07 20:23:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/82/Demo82.java
+++ JAVA-BASICS/java-basic-150/82/Demo82.java
@@ -1,3 +1,9 @@
+/*82. Write a Java program to find the largest element between the first, last, and middle values in an array of integers (even length).
+Sample Output:
+
+Original Array: [20, 30, 40, 50, 67]                                   
+Largest element between first, last, and middle values: 67 */
+
 public class Demo82{
     public static void main(String[] args) {
         System.out.println("hello");
