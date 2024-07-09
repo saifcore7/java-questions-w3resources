5dd913a64bb65e02b4d6904d834c096023475b04
saifcore7 <saif7862254j@gmail.com>
2024-07-09 19:50:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/94/Demo94.java
+++ JAVA-BASICS/java-basic-150/94/Demo94.java
@@ -1,3 +1,7 @@
+/*94. Write a Java program to rearrange all the elements of a 
+given array of integers so that all the odd numbers come 
+before all the even numbers.*/
+
 public class Demo94{
     public static void main(String[] args) {
         System.out.println("hello");
