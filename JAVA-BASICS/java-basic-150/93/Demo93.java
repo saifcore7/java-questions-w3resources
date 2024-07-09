edb2ee3aae5e988f751721e727662b8f314d046a
saifcore7 <saif7862254j@gmail.com>
2024-07-09 19:04:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/93/Demo93.java
+++ JAVA-BASICS/java-basic-150/93/Demo93.java
@@ -5,8 +5,7 @@ public class Demo93{
     public static void main(String[] args) {
         int[] a = {2,3,20,20,10,10,34};
         for(int i = 0; i < a.length - 1; i++) {
-            if(a[i] == a[i + 1] && (a[i] == 10 || a[i] == 20) && 
-                (a[i + 1] == 10 && a[i + 1] == 20) && a[i] == 10 && a[i] == 20) 
+            if(a[i] == a[i + 1] && (a[i] == 10 || a[i] == 20)) 
                 System.out.println("True");
         }
     }
