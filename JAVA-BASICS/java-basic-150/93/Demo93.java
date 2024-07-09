67321d2cfb24a6e11616434d5a83e871375c1738
saifcore7 <saif7862254j@gmail.com>
2024-07-09 18:41:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/93/Demo93.java
+++ JAVA-BASICS/java-basic-150/93/Demo93.java
@@ -3,6 +3,7 @@ element 10 next to 10 or an element 20 next to 20, but not both*/
 
 public class Demo93{
     public static void main(String[] args) {
-        System.out.println("hello");
+        int[] a = [2,3,10,10,34];
+        
     }
 }
