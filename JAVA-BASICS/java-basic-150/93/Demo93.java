f789bae57eb2ce7684262110a5142a34c9456fcf
saifcore7 <saif7862254j@gmail.com>
2024-07-09 19:03:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/93/Demo93.java
+++ JAVA-BASICS/java-basic-150/93/Demo93.java
@@ -3,12 +3,11 @@ element 10 next to 10 or an element 20 next to 20, but not both*/
 
 public class Demo93{
     public static void main(String[] args) {
-        int[] a = {2,3,20,34};
+        int[] a = {2,3,20,20,10,10,34};
         for(int i = 0; i < a.length - 1; i++) {
             if(a[i] == a[i + 1] && (a[i] == 10 || a[i] == 20) && 
                 (a[i + 1] == 10 && a[i + 1] == 20) && a[i] == 10 && a[i] == 20) 
                 System.out.println("True");
-            else System.out.println("False");
         }
     }
 }
