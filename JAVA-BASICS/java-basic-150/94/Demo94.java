fa3f258ba70b4c29586a8f265211a4aa8bb93780
saifcore7 <saif7862254j@gmail.com>
2024-07-09 19:57:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/94/Demo94.java
+++ JAVA-BASICS/java-basic-150/94/Demo94.java
@@ -8,11 +8,14 @@ public class Demo94{
         int a[] = {1,2,3,4,5,6,7,8,9,10};
         int[] even = new int[10];
         int[] odd = new int[10];
+        int j = 0,k=0;
         for(int i = 0; i < a.length; i++) {
             if(a[i] % 2 != 0) {
-                odd[i] = a[i];
+                odd[j] = a[i];
+                j++;
             } else {
-                even[i] = a[i]; 
+                even[k] = a[i];
+                k++;
             }
         }
         System.out.println(Arrays.toString(even));
