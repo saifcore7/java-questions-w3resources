fe02dcaff0c2bcc58b5dbdc1bc8de24bea7519c6
saifcore7 <saif7862254j@gmail.com>
2024-07-09 18:38:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/93/Demo93.java
+++ JAVA-BASICS/java-basic-150/93/Demo93.java
@@ -1,3 +1,6 @@
+/*93. Write a Java program to test if an array of integers contains an 
+element 10 next to 10 or an element 20 next to 20, but not both*/
+
 public class Demo93{
     public static void main(String[] args) {
         System.out.println("hello");
