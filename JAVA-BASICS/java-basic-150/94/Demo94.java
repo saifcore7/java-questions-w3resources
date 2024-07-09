0d43ce69e6aef7f5af37328a19755c5f85146aa2
saifcore7 <saif7862254j@gmail.com>
2024-07-09 19:58:01 +0530

    commit

--- JAVA-BASICS/java-basic-150/94/Demo94.java
+++ JAVA-BASICS/java-basic-150/94/Demo94.java
@@ -6,8 +6,8 @@ import java.util.*;
 public class Demo94{
     public static void main(String[] args) {
         int a[] = {1,2,3,4,5,6,7,8,9,10};
-        int[] even = new int[10];
-        int[] odd = new int[10];
+        int[] even = new int[5];
+        int[] odd = new int[5];
         int j = 0,k=0;
         for(int i = 0; i < a.length; i++) {
             if(a[i] % 2 != 0) {
